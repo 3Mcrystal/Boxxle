e103681dea72a50d6e809686d3be2b1a58f826ed
gyael <yael.girard@ynov.com>
2024-03-16 00:47:13 +0100

    assets files + draw function

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { Levels } from "./level.js";
+
 const GRID_WIDTH = 50;
 const GRID_HEIGHT = 25;
 const fps = 10
@@ -9,4 +11,29 @@ const keys = {
 }
 
 const draw = () => {
+  myGrid.innerHTML = "";
+  for (let i of tab) {
+    for (let j of i) {
+      let img = document.createElement("img");
+
+      if (j == 0) {
+      } else if (j == 1) {
+        img.src = "./assets/wall.png";
+        img.classList.add("wall");
+      } else if (j == 2) {
+        img.src = "./assets/box.png";
+        img.classList.add("box");
+      } else if (j == 3) {
+        img.src = "./assets/Aria.gif";
+        img.classList.add("Aria");
+      } else if (j == 4) {
+        img.src = "./assets/spot.png";
+        img.classList.add("spot");
+      } else if (j == 5) {
+        img.src = "./assets/boxOK.png";
+        img.classList.add("boxOK");
+      }
+      myGrid.appendChild(img);
+    }
   }
+}
